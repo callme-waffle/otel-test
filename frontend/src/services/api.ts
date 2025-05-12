a8c2fe85261925cedb41457b110fab071e31a4aa
nate2402 <nate2402@ajou.ac.kr>
2025-05-12 18:12:51 +0900

    test(fe): change api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://otel_test.aoldacloud.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://oteltest.aoldacloud.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
